e996600245e0aebb60680870ba36688c9732bcaa
badinkajink <williamxie00@gmail.com>
2024-10-06 01:40:39 -0600

    don't pad timesteps anymore...that was handled automatically

--- deligrasp_dataset/deligrasp_dataset_dataset_builder.py
+++ deligrasp_dataset/deligrasp_dataset_dataset_builder.py
@@ -44,7 +44,32 @@ class DeligraspDataset(tfds.core.GeneratorBasedBuilder):
                                 '6x end-effector position (x,y,z,rx,ry,rz relative to base frame), '
                                 '1x gripper position, 1x gripper applied force, ' 
                                 '1x gripper contact force, 1x action_blocked flag].',
-                        )
+                        ),
+                        'cartesian_position': tfds.features.Tensor( # this should be ee_pose but I'm following DROID
+                            shape=(6,),
+                            dtype=np.float64,
+                            doc='6x end-effector pose (x,y,z,rx,ry,rz relative to base frame)',
+                        ),
+                        'joint_position': tfds.features.Tensor( # this should be ee_pose but I'm following DROID
+                            shape=(6,),
+                            dtype=np.float64,
+                            doc='UR5 6DoF joint positions (q0, q1, q2, q3, q4, q5)',
+                        ),                      
+                        'gripper_position': tfds.features.Tensor(
+                            shape=(1,),
+                            dtype=np.float64,
+                            doc='gripper position',
+                        ),
+                        'applied_force': tfds.features.Tensor(
+                            shape=(1,),
+                            dtype=np.float64,
+                            doc='gripper applied force',
+                        ),
+                        'contact_force': tfds.features.Tensor(
+                            shape=(1,),
+                            dtype=np.float64,
+                            doc='gripper measured contact force',
+                        ),
                     }),
                     'action': tfds.features.Tensor(
                         shape=(9,),
@@ -53,6 +78,11 @@ class DeligraspDataset(tfds.core.GeneratorBasedBuilder):
                             '1x delta gripper position, 1x delta gripper applied force, 1x terminate episode].',
                     ),
                     'action_dict': tfds.features.FeaturesDict({
+                        'cartesian_position': tfds.features.Tensor(
+                            shape=(6,),
+                            dtype=np.float64,
+                            doc='end effector pose delta, relative to base frame',
+                        ),
                         'translation': tfds.features.Tensor(
                             shape=(3,),
                             dtype=np.float64,
@@ -63,14 +93,16 @@ class DeligraspDataset(tfds.core.GeneratorBasedBuilder):
                             dtype=np.float64,
                             doc='end effector rotation delta, relative to base frame',
                         ),
-                        'gripper_position': tfds.features.Scalar(
+                        'gripper_position': tfds.features.Tensor(
+                            shape=(1,),
                             dtype=np.float64,
                             doc='gripper position delta.',
                         ),
-                        'gripper_force': tfds.features.Scalar(
+                        'gripper_force': tfds.features.Tensor(
+                            shape=(1,),
                             dtype=np.float64,
                             doc='gripper force delta.',
-                        )
+                        ),
                     }),
                     'discount': tfds.features.Scalar(
                         dtype=np.float32,
@@ -144,18 +176,25 @@ class DeligraspDataset(tfds.core.GeneratorBasedBuilder):
                                   step['aperture'], step['applied_force'], step['contact_force'], False]) # action_blocked is always False
                 action = np.array([step['dx'], step['dy'], step['dz'], step['drx'], step['dry'], step['drz'],
                                    step['d_aperture'], step['d_applied_force'], i == (len(data) - 1)]) # terminate episode is 1 on last step
+                grippper_position = np.array([step['aperture']])
                 episode.append({
                     'observation': {
                         'image': step['img'],
                         'wrist_image': step['wrist_img'],
                         'state': state,
+                        'cartesian_position': np.array([step['x'], step['y'], step['z'], step['rx'], step['ry'], step['rz']]),
+                        'joint_position': np.array([step['q0'], step['q1'], step['q2'], step['q3'], step['q4'], step['q5']]),
+                        'gripper_position': np.array([step['aperture']]),
+                        'applied_force': np.array([step['applied_force']]),
+                        'contact_force': np.array([step['contact_force']]),
                     },
                     'action': action,
                     'action_dict': {
-                        'translation': action[:3],
-                        'rotation': action[3:6],
-                        'gripper_position': action[6],
-                        'gripper_force': action[7],
+                        'cartesian_position': np.array([step['dx'], step['dy'], step['dz'], step['drx'], step['dry'], step['drz']]),
+                        'translation': np.array([step['dx'], step['dy'], step['dz']]),
+                        'rotation': np.array([step['drx'], step['dry'], step['drz']]),
+                        'gripper_position': np.array([step['d_aperture']]),
+                        'gripper_force': np.array([step['d_applied_force']]),
                     },
                     'discount': 1.0,
                     'reward': float(i == (len(data) - 1)),
@@ -168,16 +207,18 @@ class DeligraspDataset(tfds.core.GeneratorBasedBuilder):
                     'language_embedding': language_embedding,
                 })
                 ctr += 1
+                # break
             # if ctr < 50, we're going to pad the episode with duplicates of the last step, with all d_ terms zeroed out
             last_step = episode[-1].copy()
             last_step['action'] = np.zeros_like(last_step['action'])
+            last_step['action_dict']['cartesian_position'] = np.zeros_like(last_step['action_dict']['cartesian_position'])
             last_step['action_dict']['translation'] = np.zeros_like(last_step['action_dict']['translation'])
             last_step['action_dict']['rotation'] = np.zeros_like(last_step['action_dict']['rotation'])
-            last_step['action_dict']['gripper_position'] = 0.0
-            last_step['action_dict']['gripper_force'] = 0.0
+            last_step['action_dict']['gripper_position'] = np.array([0.0])
+            last_step['action_dict']['gripper_force'] = np.array([0.0])
             pad_len = 50 - ctr
-            for _ in range(pad_len):
-                episode.append(last_step.copy())            
+            # for _ in range(pad_len):
+            #     episode.append(last_step.copy())            
             sample = {
                 'steps': episode,
                 'episode_metadata': {
@@ -194,7 +235,7 @@ class DeligraspDataset(tfds.core.GeneratorBasedBuilder):
         # for smallish datasets, use single-thread parsing
         for sample in episode_paths:
             yield _parse_example(sample)
-
+            # break
         # for large datasets use beam to parallelize data parsing (this will have initialization overhead)
         # beam = tfds.core.lazy_imports.apache_beam
         # return (
